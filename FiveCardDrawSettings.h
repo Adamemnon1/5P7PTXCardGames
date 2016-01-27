1b1e58ce6ee797b37fdc17049bdaf04ddb4348d3
Adam <ajsyav@gmail.com>
2016-01-27 13:35:03 -0600

    Create FiveCardDrawSettings.h

---
+++ FiveCardDrawSettings.h
@@ -0,0 +1,35 @@
+#ifndef __FiveCardDrawSettings_
+#define __FiveCardDrawSettings_
+#include <string>
+
+using namespace std;
+string yesResponse = "yes";
+string noResponse = "no";
+string playerlabel = "Player: ";
+string handlabel = "Hand: ";
+string whichcards = "Which cards would player ";
+string liketodiscard = " like to discard? List the indices of the cards you would like to discard (indices start at 0).";
+string inputlabel = ">> ";
+string usurebro = "Are you sure you do not want to discard anything? Enter 'yes' or 'no' please. ";
+string validindices = "Please enter valid indices from 0 to ";
+string repeatedindex = "Repeated index";
+string waterudoing = "Please enter an integer";
+string winround = " wins this round!";
+string winlabel = "Wins: ";
+string losslabel = "Losses: ";
+string leavegamebro = "Do any players want to LEAVE the game? Enter \"yes\" or \"no\".";
+string whosleaving = "Enter the name of the player that is leaving.";
+string whoutalkingbout = "There is no player with the name ";
+string period = ".";
+string outerspace = " ";
+string whoscoming = "Do any players want to ENTER the game? Enter \"yes\" or \"no\".";
+string whatshisnamebro = "Enter the name of the player that is entering.";
+
+string howryabettinFirst = ", What would you like to do? Bet, or Check? Input b or c.";
+string howryabettinSecond = ", What would you like to do? Fold, Call, or Raise? Input f, c, or r.";
+string howmuchbetbro = "How much would you like to bet? Input 1 or 2.";
+string howmuchraisebro = "How much would you like to raise? Input 1 or 2";
+string winpot = ", has won the pot of ";
+string chipslabel = "Chips: ";
+string howryabettincheapscate = " You do not have enough chips to raise. You may call or fold. Input 'f' or 'c'.";
+#endif
